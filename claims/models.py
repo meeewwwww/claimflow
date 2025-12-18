14b7bdc2a496436ec2b2e422483e4a59907ac428
Anna Demchenko <dav_1000@mail.ru>
2025-12-18 17:50:17 +0300

    Added Claim and Flight Models

--- claims/models.py
+++ claims/models.py
@@ -1,3 +1,23 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+from employee.models import Flight
+
+
+class Claim(models.Model):
+    STATUSES = (
+        ('in_progress', 'На рассмотрении'),
+        ('completed', 'Рассмотрена'),
+    )
+
+    name = models.CharField(max_length=25, verbose_name='Имя', help_text='Имя пассажира')
+    surname = models.CharField(max_length=50, verbose_name='Фамилия', help_text='Фамилия пассажира')
+    booking_reference = models.CharField(unique=True, validators=[RegexValidator(r'[A-Za-Z0-9]{8}')],
+                                         verbose_name='Код бронирования',
+                                         help_text='Код бронирования состоит из 8 символов (латинские буквы и цифры)')
+    status = models.CharField(choices=STATUSES, default=STATUSES[0][1], verbose_name='Статус')
+    flight_number = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='claims')
+    email = models.EmailField()
+    claim = models.TextField()
+    attachment = models.FileField(upload_to='claim_attachment/%Y/%m/%d', default=None, blank=True,
+                                  null=True, verbose_name='Приложение', help_text='Приложение должно быть одним файлом')
--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,13 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Flight(models.Model):
+    flight_number = models.CharField(validators=[RegexValidator(r'CF[0-9]{4}')], verbose_name='Номер рейса',
+                                     help_text='Номер рейса состоит из кода компании и четырех цифр, например, CF1234')
+    departure_time_plan = models.TimeField(verbose_name='Планируемое время вылета')
+    departure_time_fact = models.TimeField(verbose_name='Фактическое время вылета')
+    arrival_time_plan = models.TimeField(verbose_name='Планируемое время прилета')
+    arrival_time_fact = models.TimeField(verbose_name='Фактическое время прилета')
+    comment = models.TextField(blank=True, null=True, verbose_name='Комментарий к рейсу',
+                               help_text='Введите причину задержки вылета, прилета рейса или иные комментарии')
