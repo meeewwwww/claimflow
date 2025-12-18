c5405a2a4656a5925cc203aefc06032402df45ca
Anna Demchenko <dav_1000@mail.ru>
2025-12-18 17:53:51 +0300

    Added view classes: About, Contacts

--- core/views.py
+++ core/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from core.utils import DefaultDataMixin
 
-title = 'ClaimFlow - Система урегулирования претензий'
 
-
-class MainPage(TemplateView):
+class MainPage(DefaultDataMixin, TemplateView):
     template_name = 'core/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['title'] = title
+        for item in self.additional_data:
+            context[item] = self.additional_data[item]
+        return context
+
+
+# Создать шаблон
+class About(DefaultDataMixin, TemplateView):
+    template_name = 'core/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        for item in self.additional_data:
+            context[item] = self.additional_data[item]
+        return context
+
+
+# Создать шаблон
+class Contacts(DefaultDataMixin, TemplateView):
+    template_name = 'core/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        for item in self.additional_data:
+            context[item] = self.additional_data[item]
         return context
