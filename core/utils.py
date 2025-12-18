446cfac94783c6b359e13c75e3a0ed0f0d45830c
Anna Demchenko <dav_1000@mail.ru>
2025-12-18 17:42:54 +0300

    Added utils.py for default information

---
+++ core/utils.py
@@ -0,0 +1,7 @@
+class DefaultDataMixin:
+    additional_data = {
+        'title': 'ClaimFlow - Система урегулирования претензий',
+        'menu': [{'title': 'Главная', 'url_name': 'start_page'},
+                 {'title': 'О нас', 'url_name': 'about'},
+                 {'title': 'Контакты', 'url_name': 'contacts'}],
+    }
