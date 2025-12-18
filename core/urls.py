6b352f9524364c2e8812645d31521be51c045fcb
Anna Demchenko <dav_1000@mail.ru>
2025-12-18 17:52:53 +0300

    Added urls: about, contacts

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from core.views import MainPage
 
 urlpatterns = [
-    path('', MainPage.as_view(), name='start-page'),
+    path('', MainPage.as_view(), name='start_page'),
+    path('about/', MainPage.as_view(), name='about'),
+    path('contacts/', MainPage.as_view(), name='contacts'),
 ]
