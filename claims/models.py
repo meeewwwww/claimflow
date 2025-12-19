fc748f5355dbfe0a3b991b443d1c2cbb9a0077c5
Anna Demchenko <dav_1000@mail.ru>
2025-12-19 13:35:00 +0300

    Added RegexValidator for name, surname fields. Added __str__ method

--- claims/models.py
+++ claims/models.py
@@ -10,14 +10,20 @@ class Claim(models.Model):
         ('completed', 'Рассмотрена'),
     )
 
-    name = models.CharField(max_length=25, verbose_name='Имя', help_text='Имя пассажира')
-    surname = models.CharField(max_length=50, verbose_name='Фамилия', help_text='Фамилия пассажира')
-    booking_reference = models.CharField(unique=True, validators=[RegexValidator(r'[A-Za-z0-9]{8}')],
+    name = models.CharField(max_length=25, validators=[RegexValidator(r'[A-Z]+')],
+                            verbose_name='Имя', help_text='Имя пассажира как в загранпаспорте')
+    surname = models.CharField(max_length=50,  validators=[RegexValidator(r'[A-Z]+')],
+                               verbose_name='Фамилия', help_text='Фамилия пассажира как в загранпаспорте')
+    booking_reference = models.CharField(unique=True, validators=[RegexValidator(r'[A-Z0-9]{8}')],
                                          verbose_name='Код бронирования',
                                          help_text='Код бронирования состоит из 8 символов (латинские буквы и цифры)')
     status = models.CharField(choices=STATUSES, default=STATUSES[0][1], verbose_name='Статус')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата поступления претензии')
     flight_number = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='claims')
     email = models.EmailField()
     claim = models.TextField()
     attachment = models.FileField(upload_to='claim_attachment/%Y/%m/%d', default=None, blank=True,
                                   null=True, verbose_name='Приложение', help_text='Приложение должно быть одним файлом')
+
+    def __str__(self):
+        return f'{self.flight_number}, {self.booking_reference}, {self.created.strftime('%d %b %H:%M')}'
