30d6d82960e58917dfafd523178c6b270a0802bd
Anna Demchenko <dav_1000@mail.ru>
2025-12-21 16:11:43 +0300

    Added DelayCalculator logic for calculating delay time and a proper compensation

--- passenger/urls.py
+++ passenger/urls.py
@@ -7,6 +7,6 @@ app_name = 'passenger'
 
 urlpatterns = [
     path('send_claim/', SendClaim.as_view(), name='send_claim'),
-    path('success/', SendClaimSuccess.as_view(), name='success_send_claim'),
+    path('success/<str:booking_reference>/', SendClaimSuccess.as_view(), name='success_send_claim'),
     path('fail/', SendClaimFail.as_view(), name='fail_send_claim'),
 ]
--- passenger/views.py
+++ passenger/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
+from claims.models import Claim
+from core.models import Flight
+from core.services import DelayCalculator
 from core.utils import DefaultDataMixin
-from employee.models import Flight
 from passenger.forms import ClaimForm
 
 
@@ -14,6 +15,8 @@ class SendClaim(DefaultDataMixin, FormView):
 
     def form_valid(self, form):
         form.save()
+        flight_obj = form.cleaned_data['flight_number']
+        flight_obj.time_of_delay = DelayCalculator.calculate_delay_time(flight_obj)
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
@@ -25,10 +28,25 @@ class SendClaim(DefaultDataMixin, FormView):
         context['title'] = 'ClaimFlow - Подача претензии'
         return context
 
+    def get_success_url(self):
+        booking_reference = self.request.POST['booking_reference']
+        success_url = reverse_lazy('passenger:success_send_claim', kwargs={'booking_reference': booking_reference})
+        return success_url
 
-class SendClaimSuccess(TemplateView):
+
+class SendClaimSuccess(DefaultDataMixin, TemplateView):
     template_name = 'passenger/send_claim_success.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        for item in self.additional_data:
+            context[item] = self.additional_data[item]
+        booking_reference = context['booking_reference']
+        flight_object = Claim.objects.get(booking_reference=booking_reference).flight_number
+        context['time_of_delay'] = flight_object.time_of_delay
+        context['compensation'] = DelayCalculator.calculate_delay_compensation(flight_object)
+        return super().get_context_data(**context)
 
-class SendClaimFail(TemplateView):
+
+class SendClaimFail(DefaultDataMixin, TemplateView):
     template_name = 'passenger/send_claim_fail.html'
