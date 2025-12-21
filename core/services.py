5c2ae9c17a6618b235514175b8faa6bbd3c27459
Anna Demchenko <dav_1000@mail.ru>
2025-12-21 16:33:53 +0300

    Compensations texts put it constants.py

--- core/services.py
+++ core/services.py
@@ -1,3 +1,4 @@
+from core.constants import DELAY_COMPENSATIONS
 from core.models import Flight
 
 
@@ -24,23 +25,13 @@ class DelayCalculator:
     def calculate_delay_compensation(cls, flight):
         if not isinstance(flight, Flight):
             raise TypeError(f"{cls.__name__} only accepts objects of type 'Flight'")
-        default = [
-            'В соответствии с ФАП-82 Вы имеете право на бесплатное:',
-            'организацию хранения багажа;',
-            'предоставление комнаты матери и ребенка пассажиру с ребенком в возрасте до 7 лет;'
-        ]
         delay_hours = flight.time_of_delay.seconds / 3600
         if delay_hours >= 6:
-            default += ['размещение в гостинице при ожидании отправления рейса более восьми часов - в дневное время и более шести часов - в ночное время, а также доставку пассажиров транспортом от аэропорта до гостиницы и обратно.']
-            return default
+            return DELAY_COMPENSATIONS['6_hours']
         if delay_hours >= 4:
-            default += ['обеспечение горячим питанием при ожидании отправления рейса более четырех часов. При дальнейшей задержке рейса питание предоставляется каждые 6 часов в дневное время и каждые 8 часов в ночное время.']
-            return default
+            return DELAY_COMPENSATIONS['4_hours']
         if delay_hours >= 2:
-            default += ['обеспечение прохладительными напитками, а также 2 телефонных звонка или 2 сообщения по электронной почте при ожидании отправления рейса более 2-х часов.']
-            return default
+            return DELAY_COMPENSATIONS['2_hours']
         if delay_hours >= 0:
-            default[-1] = '- предоставление комнаты матери и ребенка пассажиру с ребенком в возрасте до 7 лет.'
-            return default
-        else:
-            default = []
+            return DELAY_COMPENSATIONS['less_than_2_hours']
+        return DELAY_COMPENSATIONS['no_delay']
